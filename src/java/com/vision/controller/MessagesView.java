21ec517930c858800d44e2daa4ab2c40a3f5cb18
aiturrizaga <aiturrizaga@vallegrande.edu.pe>
2018-10-10 20:50:33 -0500

    Vision template init...!!!

---
+++ src/java/com/vision/controller/MessagesView.java
@@ -0,0 +1,30 @@
+
+package com.vision.controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named(value = "messagesView")
+@SessionScoped
+public class MessagesView implements Serializable {
+
+    public void info() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "PrimeFaces Rocks."));
+    }
+
+    public void warn() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning!", "Watch out for PrimeFaces."));
+    }
+
+    public void error() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Contact admin."));
+    }
+
+    public void fatal() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Fatal!", "System Error"));
+    }
+
+}
