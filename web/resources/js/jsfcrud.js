f4764c135f6b0067cc4027190308a66365c09795
aiturrizaga <aiturrizaga@vallegrande.edu.pe>
2018-10-11 15:17:49 -0500

    Progress loader(aprte II)

--- web/resources/js/jsfcrud.js
+++ web/resources/js/jsfcrud.js
@@ -2,7 +2,7 @@
 function handleSubmit(args, dialog) {
     var jqDialog = jQuery('#' + dialog);
     if (args.validationFailed) {
-        jqDialog.effect('shake', {times: 3}, 100);
+        jqDialog.effect('shake', { times: 3 }, 100);
     } else {
         PF(dialog).hide();
     }
@@ -10,18 +10,11 @@ function handleSubmit(args, dialog) {
 
 
 /*progress*/
-$('body').show();
-$('.version').text(NProgress.version);
-NProgress.start();
-setTimeout(function () {
-    NProgress.done();
-    $('.fade').removeClass('out');
-}, 1000);
-
 window.onload = function () {
+    $('body').show();
     NProgress.start();
-    var contenedor = document.getElementById('contenedor_carga');
-    contenedor.style.visibility = 'hidden';
-    contenedor.style.opacity = '0';
-    NProgress.done();
+    setTimeout(function () {
+        NProgress.done();
+        $('.fade').removeClass('out');
+    }, 500);
 }
