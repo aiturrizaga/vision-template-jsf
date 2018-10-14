9ffeccd12b5a06d16a9ce48344c6f2c7afabeacd
aiturrizaga <aiturrizaga@vallegrande.edu.pe>
2018-10-14 12:54:39 -0500

    Loader dentro del contenido

---
+++ web/resources/js/script.js
@@ -0,0 +1,6 @@
+/*La animacion dejara de girar cuando carge la pagina*/
+window.onload = function(){
+	var contenedor = document.getElementById('contenedor_carga');
+	contenedor.style.visibility = 'hidden';
+    contenedor.style.opacity = '0';
+}
